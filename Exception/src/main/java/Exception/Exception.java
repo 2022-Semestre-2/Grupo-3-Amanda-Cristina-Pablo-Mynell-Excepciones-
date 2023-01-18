c5802a55aa1214fc1a4d72462df8ac711e7195a3
Mynell2022 <99918599+Mynell2022@users.noreply.github.com>
2023-01-18 10:31:14 -0600

    Update Exception.java

--- Exception/src/main/java/Exception/Exception.java
+++ Exception/src/main/java/Exception/Exception.java
@@ -12,41 +12,13 @@ public class Exception {
     public static void main(String[] args){
         try{
         new Exception().error();
-<<<<<<< HEAD
-        
-        /*
-        EJEMPLO TRY...CATCH CON EXCEPCION ARITMETICA
-        */
-        int numero1;
-        int numero2;
-    
-        
-        try{
-    
-            numero1 = 2;
-            numero2 = 0;
-            int resultado = numero1/numero2;
-        
-        }
-        catch(ArithmeticException e){
-        
-            System.out.println("Excepcion de tipo aritmetica, no se puede dividir un numero entre 0");
-    
-        }
-        
-    }
-
-    public void error(){
-=======
         }catch(ArithmeticException e){
             e.printStackTrace();
         }
     }
     public void error() throws ArithmeticException {
->>>>>>> be2328ee7c72e217460c000c0cfd7a9df57cad13
         int i= 10/0;
         System.out.println("Imposible ="+i);
     }
-    
-    
 }
+
