7db5117ebbc8d28135802d7909b77fe7275c35d8
Mynell2022 <99918599+Mynell2022@users.noreply.github.com>
2023-01-18 10:44:50 -0600

    Update Exception.java
    Mantengase asi, este es un programa simple sobre excepciones

--- Exception/src/main/java/Exception/Exception.java
+++ Exception/src/main/java/Exception/Exception.java
@@ -1,32 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Exception;
 
-/**
- *
- * @author Usuario
- */
 public class Exception {
     public static void main(String[] args){
         try{
         new Exception().error();
-
-
-    }
-
-    public void error(){
-
-
         }catch(ArithmeticException e){
-            e.printStackTrace();
+            System.out.println("Error: No se puede dividir entre 0");
         }finally{
             System.out.println("Terminado");
         }
     }
     public void error() throws ArithmeticException {
-        int i= 10/0;
-        System.out.println("Imposible ="+i);
+        int i= 10/1;
+        System.out.println("Resultado ="+i);
     }
 }
